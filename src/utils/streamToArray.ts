cfa3644c7cfa11db7eccb73ebc72cf6cd37376e3
Ivan Marcellino <ivan.marcellino@gdplabs.id>
2022-01-18 16:32:35 +0700

    prepwork for object storage: streamToArray & streamToString helpers

---
+++ src/utils/streamToArray.ts
@@ -0,0 +1,33 @@
+import { Readable as ReadableStream } from 'stream';
+
+export function streamToArray(stream: ReadableStream): Promise<any[]> {
+  return new Promise((resolve, reject) => {
+    const array = [];
+    stream.on('data', onData);
+    stream.on('end', onEnd);
+    stream.on('error', onEnd);
+    stream.on('close', onClose);
+
+    function onData(data) {
+      array.push(data);
+    }
+
+    function onEnd(error) {
+      if (error) reject(error);
+      else resolve(array);
+      cleanup();
+    }
+
+    function onClose() {
+      resolve(array);
+      cleanup();
+    }
+
+    function cleanup() {
+      stream.removeListener('data', onData);
+      stream.removeListener('end', onEnd);
+      stream.removeListener('error', onEnd);
+      stream.removeListener('close', onClose);
+    }
+  });
+}
