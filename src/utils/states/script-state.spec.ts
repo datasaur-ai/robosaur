911f5cf1a7be139a5137512b05d3f1f14a440182
Ivan Marcellino <ivan.marcellino@gdplabs.id>
2022-01-24 11:01:49 +0700

    enhancement: script-state unit tests
    - test projectNameHasBeenUsed logic: checks both name and status
    - test addProject: must only add to current active team

---
+++ src/utils/states/script-state.spec.ts
@@ -0,0 +1,88 @@
+import { JobStatus } from '../../datasaur/get-jobs';
+import { ScriptState } from './script-state';
+import { ProjectState, TeamProjectsState } from './team-projects-state';
+
+describe(ScriptState.name, () => {
+  describe(ScriptState.prototype.projectNameHasBeenUsed, () => {
+    let state: ScriptState;
+    beforeEach(() => {
+      state = new ScriptState();
+      jest.spyOn(TeamProjectsState.prototype, 'getProjects').mockReturnValue([
+        {
+          projectName: 'delivered-project',
+          status: JobStatus.DELIVERED,
+        } as ProjectState,
+        {
+          projectName: 'inprogress-project',
+          status: JobStatus.IN_PROGRESS,
+        } as ProjectState,
+      ]);
+      jest.spyOn(state, 'getTeamProjectsState').mockReturnValue(new TeamProjectsState('dummy-team'));
+    });
+
+    it('should return true when argument has been used as a project name', () => {
+      const output = state.projectNameHasBeenUsed('delivered-project');
+      expect(output).toBeTruthy();
+    });
+
+    it('should return false when argument has not been used as a project name', () => {
+      const output = state.projectNameHasBeenUsed('unfinished-project');
+      expect(output).toBeFalsy();
+    });
+  });
+
+  describe(ScriptState.prototype.addProject, () => {
+    let state: ScriptState;
+    const ACTIVE_TEAM = 'active-team';
+    const OTHER_TEAM = 'other-team';
+
+    beforeEach(() => {
+      state = new ScriptState();
+      jest.spyOn(state, 'getActiveTeamId').mockReturnValue(ACTIVE_TEAM);
+
+      dummyPopulateProjects(ACTIVE_TEAM, 4, state);
+      dummyPopulateProjects(OTHER_TEAM, 4, state);
+    });
+
+    it('should add project only to the current active team', () => {
+      // push project to the script state as a whole
+      // state should only push to active team state
+      const dummyNewProject = { projectName: 'newly-added-project' };
+      state.addProject(dummyNewProject as ProjectState);
+
+      expect(state.getTeamProjectsState().getProjects().length).toBeGreaterThan(
+        state.getTeamProjectsState(OTHER_TEAM).getProjects().length,
+      );
+
+      expect(
+        state
+          .getTeamProjectsState()
+          .getProjects()
+          .find((p) => p.projectName === dummyNewProject.projectName),
+      ).toBeTruthy();
+
+      expect(
+        state
+          .getTeamProjectsState(OTHER_TEAM)
+          .getProjects()
+          .find((p) => p.projectName === dummyNewProject.projectName),
+      ).toBeFalsy();
+    });
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+});
+
+function dummyPopulateProjects(teamId: string, projectCount: number, state: ScriptState) {
+  for (let index = 0; index < projectCount; index++) {
+    const identifier = `${teamId}-${index}`;
+
+    // manually get team's state, and push a project to it
+    state.getTeamProjectsState(teamId).push({
+      projectId: identifier,
+      projectName: identifier,
+    } as ProjectState);
+  }
+}
