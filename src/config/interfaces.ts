2506dddd42160ed2351b87e11bd2311f2048fbbe
Ivan Marcellino <ivan.marcellino@gdplabs.id>
2022-02-25 16:02:59 +0700

    update sample question-set and row-based quickstart

--- src/config/interfaces.ts
+++ src/config/interfaces.ts
@@ -78,6 +78,7 @@ export interface Config {
       viewerConfig?: {
         urlColumnNames: string[];
       };
+      enableTabularMarkdownParsing: boolean;
     };
     /**
      * @description Question configurations. Only applicable when documentSettings.kind is ROW_BASED
