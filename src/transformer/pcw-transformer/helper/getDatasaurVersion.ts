7698868976cb5d0ad0465732f701c9b5e73bff7c
Michael Jong <michael.jong@datasaur.ai>
2022-08-10 11:33:44 +0700

    changed version to use BE

--- src/transformer/pcw-transformer/helper/getDatasaurVersion.ts
+++ src/transformer/pcw-transformer/helper/getDatasaurVersion.ts
@@ -3,6 +3,6 @@ import { getConfig } from '../../../config/config';
 import { parseVersion } from './compareVersion';
 
 export async function getDatasaurVersion() {
-  const res = await axios.get(`${getConfig().datasaur.host}/api/front-end-configuration`);
-  return parseVersion(res.data.app.version);
+  const res = await axios.get(`${getConfig().datasaur.host}/api/health`);
+  return parseVersion(res.data.version);
 }
