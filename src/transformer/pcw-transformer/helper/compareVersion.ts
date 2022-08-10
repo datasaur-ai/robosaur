f55282b013ec3d4590e0894ebfda60acd3ef97a8
Michael Jong <michael.jong@datasaur.ai>
2022-08-10 12:00:32 +0700

    use localeCompare to compare versions

--- src/transformer/pcw-transformer/helper/compareVersion.ts
+++ src/transformer/pcw-transformer/helper/compareVersion.ts
@@ -1,26 +1,9 @@
 import { DatasaurVersion } from '../constants';
 
-export function isVersionGreaterThanOrEqual(v1: string, v2: string) {
-  const v1List = v1.split('.').map((e: string) => parseInt(e, 10));
-  const v2List = v2.split('.').map((e: string) => parseInt(e, 10));
-
-  for (const i in v1List) {
-    v2List[i] = v2List[i] || 0;
-    if (v1List[i] === v2List[i]) {
-      continue;
-    } else if (v1List[i] > v2List[i]) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  return v2List.length > v1List.length ? false : true;
-}
-
 export function parseVersion(version) {
   const versionValues = getVersionValues().sort((v1, v2) => v1.localeCompare(v2, undefined, { numeric: true }));
   for (const value of versionValues) {
-    if (isVersionGreaterThanOrEqual(value, version)) {
+    if (value.localeCompare(version, undefined, { numeric: true }) != -1) {
       return value;
     }
   }
