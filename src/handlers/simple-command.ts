10d1a60fbb1a4ffac9e6a60f348e894aa420106b
Billy Editiano <billy.editiano@datasaur.ai>
2022-11-02 00:28:39 +0700

    remove unnecessary testing code

--- src/handlers/simple-command.ts
+++
@@ -1,9 +0,0 @@
-import { createSimpleHandlerContext } from '../execution';
-import { getLogger } from '../logger';
-
-export const handleSimpleCommand = createSimpleHandlerContext('simple-command', _handleSimpleCommand);
-
-async function _handleSimpleCommand() {
-  getLogger().info('running simple command', { somePayloadProps: 'asdasdasd' });
-  getLogger().error('Error', { somePayloadProps: 'asdasdasd' });
-}
