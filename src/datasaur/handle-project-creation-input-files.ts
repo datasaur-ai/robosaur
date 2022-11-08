3e1a0fb0f2cc5343cfb23af21cb8cdcb6db6b02a
lucassusanto <lucassusanto@gmail.com>
2022-11-08 18:11:21 +0700

    refactor

--- src/datasaur/handle-project-creation-input-files.ts
+++ src/datasaur/handle-project-creation-input-files.ts
@@ -1,5 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { unlinkSync, writeFileSync } from 'fs';
+import { Logger } from 'winston';
+import { DataPayload } from '../database/entities/data';
 import { DocumentQueueEntity } from '../database/entities/document_queue.entity';
 import { TEAM_SURATINSTRUKSI } from '../database/entities/teams.entity';
 import { getLogger } from '../logger';
@@ -21,83 +23,112 @@ interface DocumentRecognitionResponseData {
   image_data?: string[];
 }
 
-export async function handleProjectCreationInputFiles(job: DocumentQueueEntity): Promise<void> {
-  if (!job.data) {
-    return;
+class ProjectCreationInputFilesHandler {
+  private readonly data?: DataPayload;
+  private readonly logger: Logger = getLogger();
+  private currentPage: number = -1;
+
+  constructor(private readonly job: DocumentQueueEntity) {
+    this.data = this.job.data;
   }
 
-  const {
-    HCP_URL,
-    HCP_URL_AUTH,
-    DOCUMENT_RECOGNITION_API_URL,
-    DOCUMENT_RECOGNITION_API_AUTH,
-    TEMPORARY_DIRECTORY,
-  } = process.env;
+  public async handle(): Promise<void> {
+    const totalPage = this.data?.file_page_size ?? 0;
 
-  const hcpUrl = HCP_URL ?? '';
-  const hcpUrlAuth = HCP_URL_AUTH ?? '';
-  const documentRecognitionApiUrl = DOCUMENT_RECOGNITION_API_URL ?? '';
-  const documentRecognitionApiAuth = DOCUMENT_RECOGNITION_API_AUTH ?? '';
-  const projectStoragePath = `../../${TEMPORARY_DIRECTORY}/${job.id}`;
+    for (let page = 0; page < totalPage; page++) {
+      this.logger.info(`Processing page number ${page + 1}..`);
+      this.setCurrentPage(page);
 
-  const { id: dataId, parsed_image_dir: imageDirectory, file_page_size: totalPageNumber } = job.data;
-  const logger = getLogger();
+      // Step 1: Download file
+      const downloadDocumentResponse = await this.downloadFile();
+      const localFilePath = this.localFilePath();
+      const fileContent = downloadDocumentResponse.data.content;
+      writeFileSync(localFilePath, fileContent);
 
-  for (let pageNumber = 0; pageNumber < totalPageNumber; pageNumber++) {
-    logger.info(`Processing page number ${pageNumber + 1}..`);
+      // Step 2: Document recognition API
+      const documentRecognitionResponse = await this.postDocumentRecognition(fileContent);
 
-    // Step 1: Download file
-    const fileName = `${dataId}_${pageNumber.toString().padStart(3, '0')}`;
-    const fileSubPath = `${imageDirectory}/${fileName}`;
+      // Step 3: Keep or remove the downloaded file
+      if (!this.isInstructionLetterDocument(documentRecognitionResponse)) {
+        unlinkSync(localFilePath);
+        this.logger.error(`Document is not an instruction letter`);
+        continue;
+      }
 
-    const remoteFileUrl = `${hcpUrl}/${fileSubPath}`;
-    const requestHeaders = hcpUrlAuth ? { Authorization: hcpUrlAuth } : undefined;
-    const downloadFileResponse = await axios.get<DownloadFileResponseData>(remoteFileUrl, { headers: requestHeaders });
-    if (downloadFileResponse.status !== 200) {
-      logger.error(`Download document request failed at ${remoteFileUrl}`);
+      const imageContent = documentRecognitionResponse.data.image_data?.join('') ?? '';
+      writeFileSync(localFilePath, imageContent);
+    }
+  }
+
+  private async downloadFile(): Promise<AxiosResponse<DownloadFileResponseData>> {
+    const remoteFilePath = this.remoteFilePath();
+    const authToken = process.env.HCP_URL_AUTH;
+    const headers = authToken ? { Authorization: authToken } : undefined;
+    const response = await axios.get<DownloadFileResponseData>(remoteFilePath, { headers });
+    if (response.status !== 200) {
+      this.logger.error(`Download document request failed at ${remoteFilePath}`);
       throw new Error();
     }
+    return response;
+  }
 
-    const localFilePath = `${projectStoragePath}/${fileSubPath}`;
-    const fileContent = downloadFileResponse.data.content;
-    writeFileSync(localFilePath, fileContent);
-
-    // Step 2: Document recognition API
-    const documentRecognitionRequestData: DocumentRecognitionRequestData = {
-      id: job.id.toString(),
-      key: documentRecognitionApiAuth,
+  private async postDocumentRecognition(fileContent: string): Promise<AxiosResponse<DocumentRecognitionResponseData>> {
+    const { DOCUMENT_RECOGNITION_API_URL, DOCUMENT_RECOGNITION_API_AUTH } = process.env;
+    const apiUrl = DOCUMENT_RECOGNITION_API_URL ?? '';
+    const apiAuth = DOCUMENT_RECOGNITION_API_AUTH ?? '';
+    const requestData: DocumentRecognitionRequestData = {
+      id: this.job.id.toString(),
+      key: apiAuth,
       teamId: TEAM_SURATINSTRUKSI,
       file: fileContent,
     };
-    const documentRecognitionResponse = await axios.post<DocumentRecognitionResponseData>(
-      documentRecognitionApiUrl,
-      documentRecognitionRequestData,
-    );
-    if (documentRecognitionResponse.status !== 200) {
-      logger.error(
-        `Document recognition request failed at ${documentRecognitionApiUrl} ${documentRecognitionRequestData}`,
-      );
+    const response = await axios.post<DocumentRecognitionResponseData>(apiUrl, requestData);
+    if (response.status !== 200) {
+      this.logger.error(`Document recognition request failed at ${apiUrl} ${requestData}`);
       throw new Error();
     }
+    return response;
+  }
 
-    // Step 3: Keep or remove the downloaded file
-    const { document_prediction: documentPredictions, image_data: imageData } = documentRecognitionResponse.data;
-
+  private isInstructionLetterDocument(response: AxiosResponse<DocumentRecognitionResponseData>): boolean {
+    const { document_prediction: documentPredictions, image_data: imageData } = response.data;
     if (!imageData) {
-      unlinkSync(localFilePath);
-      logger.error(`Document is not an instruction letter`);
-      throw new Error();
+      return false;
     }
-
     for (const documentPrediction of documentPredictions) {
       if (documentPrediction !== 'SURAT_INSTRUKSI') {
-        unlinkSync(localFilePath);
-        logger.error(`Document is not an instruction letter`);
-        throw new Error();
+        return false;
       }
     }
-
-    // Replace the existing file with image_data
-    writeFileSync(localFilePath, imageData.join(''));
+    return true;
   }
+
+  private localFilePath(): string {
+    const temporaryDirectoryName = process.env.TEMPORARY_DIRECTORY;
+    const projectName = this.job.id;
+    return `../../${temporaryDirectoryName}/${projectName}/${this.filePath()}`;
+  }
+
+  private remoteFilePath(): string {
+    const hcpUrl = process.env.HCP_URL ?? '';
+    return `${hcpUrl}/${this.filePath()}`;
+  }
+
+  private filePath(): string {
+    const dataId = this.data?.id ?? '';
+    const paddedPage = this.currentPage.toString().padStart(3, '0');
+    const imageDirectory = this.data?.parsed_image_dir ?? '';
+    return `${imageDirectory}/${dataId}_${paddedPage}}`;
+  }
+
+  private setCurrentPage(currentPage: number): void {
+    this.currentPage = currentPage;
+  }
+}
+
+export async function handleProjectCreationInputFiles(job: DocumentQueueEntity): Promise<void> {
+  if (!this.job.data) {
+    return;
+  }
+  await new ProjectCreationInputFilesHandler(job).handle();
 }
