0c02fa8c24bdb38f71d8c75d6cd298c7a38848ef
Michael Jong <michael.jong@datasaur.ai>
2022-11-08 12:00:40 +0700

    implement send export result to endpoint

--- src/handlers/rex-export.handler.ts
+++ src/handlers/rex-export.handler.ts
@@ -1,6 +1,7 @@
 import { DataPayload } from '../database/entities/data';
 import { handleExportProjects } from './export-projects.handler';
 import { handleSaveToDatabase } from './save-to-database.handler';
+import { sendRequestToEndpoint } from './send-request.handler';
 
 const testDataPayload: DataPayload = {
   id: 1,
@@ -14,9 +15,12 @@ const testDataPayload: DataPayload = {
 };
 
 export async function handleExport(configFile: string, data: DataPayload) {
-  // TODO: implement post processing
+  // TODO: implement post processing and delete the exported file
   await handleExportProjects(configFile, { unzip: true });
 
   // receives json format with reading_result and document_data
   await handleSaveToDatabase(configFile, data.id);
+
+  const exportEndpoint = process.env.EXPORT_ENDPOINT ?? '';
+  await sendRequestToEndpoint(data.id, exportEndpoint);
 }
---
+++ src/handlers/send-request.handler.ts
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import { Team15 } from '../database/entities/teamPayloads/team_15.entity';
+import { getRepository } from '../database/repository';
+import { getLogger } from '../logger';
+
+export async function sendRequestToEndpoint(id: number, exportEndpoint: string) {
+  const team15Repository = await getRepository(Team15);
+  const data = await team15Repository.findOneByOrFail(id);
+
+  const payload = {
+    document_data: data.document_data,
+    document_path: data.hcp_ori_document_dir,
+    transaction_id: data.id,
+    status: isOCRSuccessful(data.ocr_status) ? 'Success' : 'Failed',
+    message: {
+      indonesian: `OCR ${isOCRSuccessful(data.ocr_status) ? 'Berhasil' : 'Gagal'}`,
+      english: `OCR ${isOCRSuccessful(data.ocr_status) ? 'Success' : 'Failed'}`,
+    },
+    received_request: data.received_request,
+    start_ocr: data.start_ocr,
+    end_ocr: data.end_ocr,
+    continuous_index: data.continuous_index,
+  };
+
+  let counterRetry = 0;
+  const LIMIT_RETRY = Number(process.env.LIMIT_RETRY);
+  while (counterRetry < LIMIT_RETRY) {
+    try {
+      const response = await axios({
+        method: 'POST',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: payload,
+        url: exportEndpoint,
+        timeout: 10000,
+      });
+      return response;
+    } catch (error) {
+      if (counterRetry >= LIMIT_RETRY) {
+        getLogger().error(`reached retry limit for sending OCR results to endpoint...`, {
+          error: JSON.stringify(error),
+          message: error.message,
+        });
+        return;
+      } else {
+        getLogger().warn(`error sending request, retrying...`, {
+          error: JSON.stringify(error),
+          message: error.message,
+        });
+      }
+    }
+  }
+}
+
+function isOCRSuccessful(ocr_status) {
+  return ocr_status === 'Read';
+}
