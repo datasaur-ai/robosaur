91f9977955264d770da2c9305dad57fd2f2580c0
Hartono Sulaiman <2196304+tonoman3g@users.noreply.github.com>
2022-11-10 11:48:50 +0700

    #14268 script to create robosaur docker container artifact (#64)
    * add scaffolding scripts for creating artifact
    
    * update robosaur.env
    
    * adjust build.sh
    
    * add control scripts
    
    * adjust docker volumes
    
    * update readme

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+dockerTag=$1
+
+cd on-premise
+rm -fr build
+mkdir build
+
+cp -r scheme/default/* ./build
+
+mkdir ./build/docker-config
+mkdir ./build/docker-state
+mkdir ./build/docker-logs
+cp ./../sample/rex/config_rex.json ./build/docker-config/
+
+export CI_REGISTRY=682361690817.dkr.ecr.us-east-1.amazonaws.com
+export DOCKER_TAG=$dockerTag
+
+docker-compose -f docker-compose.build.yml build
+
+docker save datasaur/robosaur:${dockerTag} | gzip > build/datasaur-robosaur.tar.gz
+
+echo ${dockerTag} > build/version
+mv build datasaur-robosaur-${dockerTag}
+tar -czf datasaur-robosaur-${dockerTag}.tar.gz datasaur-robosaur-${dockerTag}
+rm -fr datasaur-robosaur-${dockerTag}
