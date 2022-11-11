79ce11b1537775a2364a457532aa26e370e01359
Hartono Sulaiman <hartono@datasaur.ai>
2022-11-11 16:04:45 +0700

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-npm run start -- run-consumer /home/robosaur/config/config.json
-
-while true
-do
-  echo "."
-  sleep 5
-done
+npm run start -- start-consumer /home/robosaur/config/config.json
