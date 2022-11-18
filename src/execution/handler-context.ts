783216b1a85e5df7a320b9ff58fc1a13344fab14
Billy Editiano <102159897+billy-editiano@users.noreply.github.com>
2022-11-18 10:30:17 +0700

    fix logger trancing error (#68)
    * fix logger trancing error
    
    * remove debug log

--- src/execution/handler-context.ts
+++ src/execution/handler-context.ts
@@ -1,6 +1,7 @@
 import { getLoggerService } from '../logger/index';
 import { executionNamespace } from './execution-namespace';
 import { randomUUID } from 'crypto';
+import { getExecutionValue, preventNewTraceIdGeneration } from './helpers';
 
 export interface HandlerContextCallback<T extends unknown[]> {
   (...args: T): void | Promise<void>;
@@ -17,8 +18,15 @@ export function createSimpleHandlerContext<T extends unknown[]>(
   return function (...args) {
     return executionNamespace.runPromise(async () => {
       // generate random traceId
-      const traceId = generateRandomTraceId();
-      executionNamespace.set('traceId', traceId);
+      const shouldPreventNewTraceIdGeneration = getExecutionValue('prevent-new-traceid-generation');
+
+      let traceId;
+      if (!shouldPreventNewTraceIdGeneration) {
+        traceId = generateRandomTraceId();
+        executionNamespace.set('trace-id', traceId);
+      } else {
+        traceId = getExecutionValue('trace-id');
+      }
 
       // register resolver for logging and tracing
       getLoggerService().registerResolver(() => {
@@ -29,7 +37,9 @@ export function createSimpleHandlerContext<T extends unknown[]>(
       });
 
       // run the callback
-      return callback(...args);
+      const result = await Promise.resolve(callback(...args));
+      getLoggerService().popResolver();
+      return result;
     });
   };
 }
@@ -56,11 +66,16 @@ export function createConsumerHandlerContext<T extends unknown[], U extends unkn
   });
 
   const wrappedProcessJob = (traceId: string, ...args: U) => {
-    return executionNamespace.runPromise(async () => {
+    return executionNamespace.runAndReturn(async () => {
+      preventNewTraceIdGeneration();
       executionNamespace.set('trace-id', traceId);
       return onJob(...args);
     });
   };
 
-  return (...args: T) => consumerFn(wrappedProcessJob, ...args);
+  return async (...args: T) => {
+    const result = await Promise.resolve(consumerFn(wrappedProcessJob, ...args));
+    getLoggerService().popResolver();
+    return result;
+  };
 }
--- src/execution/helpers.ts
+++ src/execution/helpers.ts
@@ -3,3 +3,7 @@ import { executionNamespace } from './execution-namespace';
 export function getExecutionValue<T>(key: string) {
   return executionNamespace.get(key) as T;
 }
+
+export function preventNewTraceIdGeneration() {
+  executionNamespace.set('prevent-new-traceid-generation', true);
+}
