7e718a40cd557d4acf51aff9007bc4ee8f9ed0f1
Hartono Sulaiman <hartono@datasaur.ai>
2022-12-19 14:15:34 +0700

    fix review

--- on-premise/scheme/default/add-channel.sh
+++ on-premise/scheme/default/add-channel.sh
@@ -2,10 +2,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 NAME=$1
-cp -r ${DIR}/docker-config ${DIR}/docker-config-${NAME}
-cp -r ${DIR}/docker-logs ${DIR}/docker-logs-${NAME}
-cp -r ${DIR}/docker-export ${DIR}/docker-export-${NAME}
-cp -r ${DIR}/docker-state ${DIR}/docker-state-${NAME}
+cp -r ${DIR}/docker-folder-template ${DIR}/docker-logs-${NAME}
+cp -r ${DIR}/docker-folder-template ${DIR}/docker-export-${NAME}
+cp -r ${DIR}/docker-folder-template ${DIR}/docker-state-${NAME}
 
 cat ${DIR}/template.txt >> ${DIR}/docker-compose.yml
 
