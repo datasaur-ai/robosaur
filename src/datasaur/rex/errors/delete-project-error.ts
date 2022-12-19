0ddfd146c5d00735aa266fbb6619a4808358d74e
Hartono Sulaiman <hartono@datasaur.ai>
2022-12-19 14:24:26 +0700

    remove delete-project error and change the OCR Status Enum

--- src/datasaur/rex/errors/delete-project-error.ts
+++
@@ -1,8 +0,0 @@
-import { OCR_STATUS } from '../interface';
-import { OcrError } from './ocr-error';
-
-export class DeleteProjectError extends OcrError {
-  constructor(error: string) {
-    super(`Error during deleting project: ${error}`, OCR_STATUS.DELETE_PROJECT_ERROR);
-  }
-}
