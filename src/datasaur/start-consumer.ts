33f1a8050ec067f455c8e63ece1eebb4c0263069
yosua1011 <yosua1011@gmail.com>
2023-02-02 10:37:12 +0700

    usign setInterval instead while on start-consumer.ts

--- src/datasaur/start-consumer.ts
+++ src/datasaur/start-consumer.ts
@@ -8,26 +8,25 @@ import { validateRecord } from './rex/validate-record';
 
 const MAX_DOCS = Number(process.env.MAX_DOCS ?? 3);
 
-export const startConsumer = async (processJob: ProcessJob<unknown[]>, teamId: number) => {
-  while (true) {
-    const sleeptime = randbetween(0, 10);
-    wait(sleeptime);
+let intervalId;
 
+export const startConsumer = async (processJob: ProcessJob<unknown[]>, teamId: number) => {
+  clearInterval(intervalId);
+  const interval = Math.floor(Math.random() * (10000 - 1000 + 1) + 1000);
+  intervalId = setInterval(async function() {
     const queueAvailable = await validateRecord(teamId, MAX_DOCS);
 
     if (!queueAvailable) {
       getLogger().info(`Team ${teamId} Max number of concurrent process is reached [MAX_DOCS: ${MAX_DOCS}]`);
-      continue;
     }
 
     const document = await dequeueDocument(teamId);
 
     if (!document) {
       // only print the log at even sleep time
-      if (sleeptime % 2 === 0) {
+      if (interval % 2 === 0) {
         getLogger().info(`Team ${teamId} No new document is found in the queue`);
       }
-      continue;
     }
 
     const saveKeepingId = document.save_keeping_id;
@@ -40,5 +39,5 @@ export const startConsumer = async (processJob: ProcessJob<unknown[]>, teamId: n
     getLogger().info(`Process Job Team ${teamId} and Save Keeping Id ${document.save_keeping_id}`);
     // trace id is using save_keeping_id, thats why teamId is the 2nd argument
     await processJob(`${document.save_keeping_id}`, teamId, saveKeeping);
-  }
+  }, interval);
 };
