d8623c2a44a6637db5c8e42596fc867264bbf6fc
yosua1011 <yosua1011@gmail.com>
2023-02-03 09:18:53 +0700

    random interval with setInterval function

--- src/datasaur/start-consumer.ts
+++ src/datasaur/start-consumer.ts
@@ -11,13 +11,15 @@ let intervalId;
 export const startConsumer = async (processJob: ProcessJob<unknown[]>, teamId: number) => {
   clearInterval(intervalId);
   const interval = Math.floor(Math.random() * (10000 - 1000 + 1) + 1000);
+  console.log(interval);
   intervalId = setInterval(async () => {
     const queueAvailable = await validateRecord(teamId, MAX_DOCS);
 
     if (!queueAvailable) {
       getLogger().info(`Team ${teamId} Max number of concurrent process is reached [MAX_DOCS: ${MAX_DOCS}]`);
+      await startConsumer(processJob, teamId);
       return;
-    } 
+    }
     const document = await dequeueDocument(teamId);
 
     if (!document) {
@@ -25,8 +27,9 @@ export const startConsumer = async (processJob: ProcessJob<unknown[]>, teamId: n
       if (interval % 2 === 0) {
         getLogger().info(`Team ${teamId} No new document is found in the queue`);
       }
+      await startConsumer(processJob, teamId);
       return;
-    } 
+    }
 
     const saveKeepingId = document.save_keeping_id;
 
@@ -38,5 +41,6 @@ export const startConsumer = async (processJob: ProcessJob<unknown[]>, teamId: n
     getLogger().info(`Process Job Team ${teamId} and Save Keeping Id ${document.save_keeping_id}`);
     // trace id is using save_keeping_id, thats why teamId is the 2nd argument
     await processJob(`${document.save_keeping_id}`, teamId, saveKeeping);
+    await startConsumer(processJob, teamId);
   }, interval);
 };
