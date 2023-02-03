2f377a28d125b5a626f6fe5a400a0ed924ac1445
Billy Editiano <billy.editiano@datasaur.ai>
2023-02-03 11:44:44 +0700

    using await sleep

--- src/datasaur/start-consumer.ts
+++ src/datasaur/start-consumer.ts
@@ -1,33 +1,31 @@
 import { ProcessJob } from '../execution';
 import { getLogger } from '../logger';
+import { random } from '../utils/random';
+import { sleep } from '../utils/sleep';
 import { createRecordAndReturnSaveKeeping } from './rex/create-record-and-return-save-keeping';
 import { dequeueDocument } from './rex/dequeue-document';
 import { validateRecord } from './rex/validate-record';
 
 const MAX_DOCS = Number(process.env.MAX_DOCS ?? 3);
 
-let intervalId;
-
 export const startConsumer = async (processJob: ProcessJob<unknown[]>, teamId: number) => {
-  clearInterval(intervalId);
-  const interval = Math.floor(Math.random() * (10000 - 1000 + 1) + 1000);
-  intervalId = setInterval(async () => {
+  while (true) {
+    const sleeptimeSecond = random(10) + 1;
     const queueAvailable = await validateRecord(teamId, MAX_DOCS);
 
     if (!queueAvailable) {
       getLogger().info(`Team ${teamId} Max number of concurrent process is reached [MAX_DOCS: ${MAX_DOCS}]`);
-      await startConsumer(processJob, teamId);
-      return;
+      continue;
     }
+
     const document = await dequeueDocument(teamId);
 
     if (!document) {
       // only print the log at even sleep time
-      if (interval % 2 === 0) {
+      if (sleeptimeSecond % 2 === 0) {
         getLogger().info(`Team ${teamId} No new document is found in the queue`);
       }
-      await startConsumer(processJob, teamId);
-      return;
+      continue;
     }
 
     const saveKeepingId = document.save_keeping_id;
@@ -40,6 +38,7 @@ export const startConsumer = async (processJob: ProcessJob<unknown[]>, teamId: n
     getLogger().info(`Process Job Team ${teamId} and Save Keeping Id ${document.save_keeping_id}`);
     // trace id is using save_keeping_id, thats why teamId is the 2nd argument
     await processJob(`${document.save_keeping_id}`, teamId, saveKeeping);
-    await startConsumer(processJob, teamId);
-  }, interval);
+
+    await sleep(sleeptimeSecond * 1000);
+  }
 };
--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,3 +1,3 @@
-export function sleep(duration: number) {
-  return new Promise((resolve) => setTimeout(resolve, duration));
+export function sleep(durationMs: number) {
+  return new Promise((resolve) => setTimeout(resolve, durationMs));
 }
