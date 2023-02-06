07ebb19281cd7ece7e3e58c47c7bd055c4a8d0ca
Billy Editiano <billy.editiano@datasaur.ai>
2023-02-06 19:15:15 +0700

    create healthcheck server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-npm run start -- start-consumer /home/robosaur/config/config.json
+# get the container IP
+export IP=`ifconfig eth0 | grep 'inet ' | awk '{print $2}'`
+export IP=`echo $IP | cut -d':' -f2`
+
+# get the service name you specified in the docker-compose.yml 
+# by a reverse DNS lookup on the IP
+export APP_SERVICE=`dig -x $IP +short | cut -d'_' -f2`
+
+# the number of replicas is equal to the A records 
+# associated with the service name
+export APP_COUNT=`dig $APP_SERVICE +short | wc -l`
+
+# extract the replica number from the same PTR entry
+export APP_INDEX=`dig -x $IP +short | sed 's/.*_\([0-9]*\)\..*/\1/'`
+ 
+echo "Running producer $APP_INDEX of $APP_COUNT"
+
+npm run start -- $COMMAND_NAME /home/robosaur/config/config.json
