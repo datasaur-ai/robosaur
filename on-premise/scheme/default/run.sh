66b7c1812b656f2f90178bab5f2ca068ca2c1b87
daniel-jeremia <96828170+daniel-jeremia@users.noreply.github.com>
2023-02-17 10:01:44 +0700

    removed grafana volume (#84)

--- on-premise/scheme/default/run.sh
+++ on-premise/scheme/default/run.sh
@@ -6,6 +6,5 @@ export DOCKER_TAG=$(cat ${DIR}/version)
 export CI_REGISTRY=682361690817.dkr.ecr.us-east-1.amazonaws.com
 
 export $(grep -v '^#' ./robosaur.env | xargs) > /dev/null
-sudo chown 10001:10001 ./volumes/loki
-sudo chown 472:472 ./volumes/grafana
+
 docker-compose --env-file ./robosaur.env -p robosaur up -d
