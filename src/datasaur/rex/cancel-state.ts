a79010339c46920f4720a3aae01dbb6476a83665
Billy Editiano <102159897+billy-editiano@users.noreply.github.com>
2023-03-08 20:42:02 +0700

    #15807 - Improve message readability (#88)
    * add id and page in message
    
    * add id to all cancel state

--- src/datasaur/rex/cancel-state.ts
+++ src/datasaur/rex/cancel-state.ts
@@ -1,11 +1,11 @@
 export enum CancelState {
-  UPDATE_IN_PROGRESS = 'Document {id} stopped after update savekeeping status to In Progress',
-  DOCUMENT_RECOGNITION = 'Document {id} stopped after document-recognition process',
-  PROJECT_CREATION = 'Document {id} stopped after project creation',
-  PROJECT_EXPORT = 'Document {id} stopped after project export',
-  TEXT_EXTRACTION = 'Document {id} stopped after text-extraction process at {page}',
-  FIELD_EXTRACTION = 'Document {id} stopped after field-extraction process',
-  POST_PROCESSING = 'Document {id} stopped after post-processing process',
+  UPDATE_IN_PROGRESS = 'Document id {id} stopped after update savekeeping status to In Progress',
+  DOCUMENT_RECOGNITION = 'Document id {id} stopped after document-recognition process',
+  PROJECT_CREATION = 'Document id {id} stopped after project creation',
+  PROJECT_EXPORT = 'Document id {id} stopped after project export',
+  TEXT_EXTRACTION = 'Document id {id} stopped after text-extraction process at page {page}',
+  FIELD_EXTRACTION = 'Document id {id} stopped after field-extraction process',
+  POST_PROCESSING = 'Document id {id} stopped after post-processing process',
 }
 
 export interface Replacer {
