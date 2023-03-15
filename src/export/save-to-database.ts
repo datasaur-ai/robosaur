a03cc0fe01be37c520e2ce89e6f3c075f70eaa66
Billy Editiano <billy.editiano@datasaur.ai>
2023-03-15 16:27:15 +0700

    remove id

--- src/export/save-to-database.ts
+++ src/export/save-to-database.ts
@@ -41,8 +41,7 @@ export async function saveExportResultsToDatabase(teamId: number, id: number) {
     });
 
     for (let i = 0; i < record.page_count; i++) {
-      const teamId = process.env.TEAM_ID;
-      const filename = teamId + '_' + id + '_' + addLeadingZeros(i, 3);
+      const filename = id + '_' + addLeadingZeros(i, 3);
       documentData[filename] = null;
       readingResult[filename] = null;
     }
