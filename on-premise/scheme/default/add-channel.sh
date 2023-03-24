57258df2f64699fdeb11d44b7eaf75bb02e692e6
yosua1011 <yosua1011@gmail.com>
2023-03-24 14:05:46 +0700

    initialize non root user to run docker container

--- on-premise/scheme/default/add-channel.sh
+++ on-premise/scheme/default/add-channel.sh
@@ -2,6 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 NAME=$1
+UID=$2
+GID=$3
 cp -r ${DIR}/docker-folder-template ${DIR}/docker-logs-${NAME}
 cp -r ${DIR}/docker-folder-template ${DIR}/docker-export-${NAME}
 cp -r ${DIR}/docker-folder-template ${DIR}/docker-state-${NAME}
@@ -9,3 +11,5 @@ cp -r ${DIR}/docker-folder-template ${DIR}/docker-state-${NAME}
 cat ${DIR}/template.txt >> ${DIR}/docker-compose.yml
 
 sed -i 's/${NAME}/'${NAME}'/g' ${DIR}/docker-compose.yml
+sed -i 's/${UID}/'${UID}'/g' ${DIR}/docker-compose.yml
+sed -i 's/${UID}/'${UID}'/g' ${DIR}/docker-compose.yml
