b6ba7e37a94ea74352a0c3ff7d24524fd0b38336
Michael Jong <100671309+michael-jong-ds@users.noreply.github.com>
2023-06-05 16:52:34 +0700

    #16640 MongoDB connection error handler on Invoice, L/C dan Instruction Letter OCR (#96)
    * added handler for mongodb network error
    
    * moved sleep duration to env

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,7 @@
 import { DataSource } from 'typeorm';
 import { getConfig } from '../config/config';
 import { getLogger } from '../logger';
+import { sleep } from '../utils/sleep';
 
 let databaseSource: DataSource;
 
@@ -24,18 +25,25 @@ export const initDatabase = () => {
 };
 
 const getDataSource = async () => {
-  if (!databaseSource.isInitialized) {
+  if (databaseSource.isInitialized) {
+    return databaseSource;
+  }
+
+  while (true) {
     try {
       await databaseSource.initialize();
       getLogger().info(`MongoDB has been initialized`);
       return databaseSource;
     } catch (err) {
-      getLogger().error(`MongoDB initialization error`, err);
-      // rethrow
-      throw err;
+      if (err.name !== 'MongoNetworkError') {
+        getLogger().error(`MongoDB initialization error`, err);
+        throw err;
+      }
+
+      getLogger().warn('MongoDB connection error, retrying connection in 3 minutes');
+      await sleep(Number(process.env.MONGODB_CONNECTION_RETRY_INTERVAL));
     }
   }
-  return databaseSource;
 };
 
 export default getDataSource;
