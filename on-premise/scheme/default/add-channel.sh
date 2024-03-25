a2fc1b06b8f1281c2acb262a89bfe2e41f5e4ca0
Lucas <lucassusanto@gmail.com>
2024-03-25 10:54:44 +0700

    #16431 Fix promtail log issue

--- on-premise/scheme/default/add-channel.sh
+++ on-premise/scheme/default/add-channel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+set -e
 
 NAME=$1
-UID=$2
-GID=$3
+CHANNEL_UID=$2
+CHANNEL_GID=$3
 cp -r ${DIR}/docker-folder-template ${DIR}/docker-logs-${NAME}
 cp -r ${DIR}/docker-folder-template ${DIR}/docker-export-${NAME}
 cp -r ${DIR}/docker-folder-template ${DIR}/docker-state-${NAME}
@@ -11,5 +12,5 @@ cp -r ${DIR}/docker-folder-template ${DIR}/docker-state-${NAME}
 cat ${DIR}/template.txt >> ${DIR}/docker-compose.yml
 
 sed -i 's/${NAME}/'${NAME}'/g' ${DIR}/docker-compose.yml
-sed -i 's/${UID}/'${UID}'/g' ${DIR}/docker-compose.yml
-sed -i 's/${GID}/'${GID}'/g' ${DIR}/docker-compose.yml
+sed -i 's/${UID}/'${CHANNEL_UID}'/g' ${DIR}/docker-compose.yml
+sed -i 's/${GID}/'${CHANNEL_GID}'/g' ${DIR}/docker-compose.yml
