b7155ca0b7d24b7a97a1dc6fe952a0fa3e982e7a
lucassusanto <lucassusanto@gmail.com>
2024-03-26 11:03:01 +0700

    #16431 Instruction Letter Promtail Cannot Write and Send Logs (#101)

--- on-premise/scheme/default/install.sh
+++ on-premise/scheme/default/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+set -e
+
 echo "Installation started"
 docker load --input ${DIR}/datasaur-robosaur.tar.gz
 docker load --input ${DIR}/rabbitmq.tar.gz
